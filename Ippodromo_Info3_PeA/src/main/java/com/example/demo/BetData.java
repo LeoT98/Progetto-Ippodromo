200b11812b81f00d707b8f6603ea276b9d48c7df
Ste2512 <ste.be@alice.it>
2021-01-09 19:06:06 +0100

    bug fixed

--- Ippodromo_Info3_PeA/src/main/java/com/example/demo/BetData.java
+++ Ippodromo_Info3_PeA/src/main/java/com/example/demo/BetData.java
@@ -6,11 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BetData {
 
-	
-	@RestController
-	public class TicketData {
 
 		@GetMapping("/nuovaScommessa")
 		public void addScommessa(@RequestParam(value = "IDCorsa", defaultValue = "m") String IDCorsa,

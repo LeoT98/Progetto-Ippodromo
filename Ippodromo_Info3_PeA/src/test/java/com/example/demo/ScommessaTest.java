18a15f695def897225ba8fd049e495fed0e566b7
Ste2512 <ste.be@alice.it>
2021-01-16 18:46:54 +0100

    first add scommessa test

--- Ippodromo_Info3_PeA/src/test/java/com/example/demo/ScommessaTest.java
+++ Ippodromo_Info3_PeA/src/test/java/com/example/demo/ScommessaTest.java
@@ -2,13 +2,77 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 class ScommessaTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getTest() {
+		Scommessa i=new Scommessa(1,2,3);
+		assertEquals(i.getIDCorsa(),1);
+		assertEquals(i.getCavallo(),2);
+		assertEquals(i.getImporto(),3);
+	}
+	
+	@Test
+	void testGetId() {
+		Scommessa i=new Scommessa(1,2,3);
+		Scommessa i2=new Scommessa(1,2,3);
+		assertEquals(i2.getId(),1);
+	}
+	
+
+	@Test
+	void aggiungiTest() {
+		
+		Scommessa m=new Scommessa(999999,2,3);
+		m.aggiungiScommessa();;
+		String st;
+		List<Scommessa> jl=new ArrayList<Scommessa>();
+		// legge il file e salva gli elementi da tenere
+		try(FileReader f = new FileReader("Quote.json")  ){
+			BufferedReader br = new BufferedReader(f);
+			 while ((st = br.readLine()) != null) {
+				    JSONObject x=new JSONObject(st);
+				    Quota i=new Quota((int)x.get("idCorsa"),(int)x.get("idCavallo"),(int)x.get("Valore"));
+				    if(i.checkID(999999)) {
+				    		jl.add(i);
+				    }
+			  }
+			 f.close();}catch(Exception e) {}
+		assertTrue(jl.get(0).checkID(999999));
+		
+		String st1;
+		ArrayList<JSONObject> jl1=new ArrayList<JSONObject>();
+		// legge il file e salva gli elementi da tenere
+		try(FileReader f = new FileReader("Quote.json")  ){
+			BufferedReader br = new BufferedReader(f);
+			 while ((st1 = br.readLine()) != null) {
+				    JSONObject x=new JSONObject(st1);
+				    if((int)x.get("idCorsa")!=999999) {
+				    		jl1.add(x);
+				    }
+			  }
+			 f.close();}catch(Exception e) {}
+		//svuota file
+		try{
+			FileWriter f=new FileWriter("Quote.json");
+			f.close();
+			}catch(Exception e){}
+		//riscrive il file
+		try{
+			FileWriter f=new FileWriter("Quote.json",true);
+			for(int c=0;c<jl1.size();c++) {
+				f.write(jl1.get(c).toString());
+				f.write("\n");
+			}	f.close();	}catch(Exception e){}
 	}
 
 }

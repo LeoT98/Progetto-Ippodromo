46a1c03e4f57272afe68d39a3d808db342d9d215
LeoT98 <50903256+LeoT98@users.noreply.github.com>
2021-01-16 17:33:02 +0100

    qwerty

--- Ippodromo_Info3_PeA/src/main/java/com/example/demo/DatiBiglietto.java
+++ Ippodromo_Info3_PeA/src/main/java/com/example/demo/DatiBiglietto.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DatiBiglietto {
 
-	@GetMapping("/aggiugniBiglietto")
-	public void aggiungiBiglietto(@RequestParam(value = "ora", defaultValue = "m") String ora,
+	@GetMapping("/aggiungiBiglietto")
+	public String aggiungiBiglietto(@RequestParam(value = "ora", defaultValue = "m") String ora,
 											  @RequestParam(value = "data", defaultValue = "1/1/1970") String data,
 											  @RequestParam(value = "zona", defaultValue = "0") String zona,
 											  @RequestParam(value = "prezzo", defaultValue ="0.0") String prezzo) {
 		Biglietto b= new Biglietto(ora,data,Integer.parseInt(zona), Double.parseDouble(prezzo)) ;
 		b.aggiungiBiglietto();
+		return "it's working";
 	}
 	
 

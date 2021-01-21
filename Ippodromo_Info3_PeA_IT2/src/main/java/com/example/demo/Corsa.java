1fe4a8c7eef697758bdfa25f1029ab7b2749c19e
LeoT98 <50903256+LeoT98@users.noreply.github.com>
2021-01-21 21:33:37 +0100

    correzioni

--- Ippodromo_Info3_PeA_IT2/src/main/java/com/example/demo/Corsa.java
+++ Ippodromo_Info3_PeA_IT2/src/main/java/com/example/demo/Corsa.java
@@ -63,34 +63,7 @@ public class Corsa {
 		}catch(Exception e){}
 	}
 	
-	/**
-	 * rimuove la corsa dal file 'corse.json'
-	 */
-	public void rimuoviCorsa() {
-		String st;
-		ArrayList<JSONObject> jl=new ArrayList<JSONObject>();
-		try(FileReader f = new FileReader("corse.json")  ){
-			BufferedReader br = new BufferedReader(f);
-			 while ((st = br.readLine()) != null) {
-				    JSONObject x=new JSONObject(st);
-				    if((int)x.get("id")!=id) {
-				    		jl.add(x);
-				    }
-			  }
-			 f.close();}catch(Exception e) {}
-		
-		try{
-			FileWriter f=new FileWriter("corse.json");
-			f.close();
-			}catch(Exception e){}
-		
-		try{
-			FileWriter f=new FileWriter("corse.json",true);
-			for(int c=0;c<jl.size();c++) {
-				f.write(jl.get(c).toString());
-				f.write("\n");
-			}	f.close();	}catch(Exception e){}
-	}
+	
 	public int getId() {
 		return id;
 	}
